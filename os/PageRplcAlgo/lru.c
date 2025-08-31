b0609431c3c9f3c1fd5a13ca6d172c91dcf19081
smokeyshawn18 <shudarsanpoudel25@gmail.com>
2025-08-31 19:49:40 +0545

    lab6

--- os/PageRplcAlgo/lru.c
+++ os/PageRplcAlgo/lru.c
@@ -1,56 +1,86 @@
 #include <stdio.h>
 
+int findLRU(int time[], int n) {
+    int i, min = time[0], pos = 0;
+    for (i = 1; i < n; i++) {
+        if (time[i] < min) {
+            min = time[i];
+            pos = i;
+        }
+    }
+    return pos;
+}
+
 int main() {
-    int i, j, k, n, f, pf = 0, count = 0;
-    int pages[30], frames[10];
+    int no_of_frames, no_of_pages, frames[10], pages[30];
+    int counter = 0, time[10], flag1, flag2, pos, i, j, page_faults = 0;
 
     printf("Enter number of pages: ");
-    scanf("%d", &n);
+    scanf("%d", &no_of_pages);
 
     printf("Enter the reference string: ");
-    for (i = 0; i < n; i++)
+    for (i = 0; i < no_of_pages; i++)
         scanf("%d", &pages[i]);
 
     printf("Enter number of frames: ");
-    scanf("%d", &f);
+    scanf("%d", &no_of_frames);
 
-    // Initialize frames as empty
-    for (i = 0; i < f; i++)
+    // Initialize frames
+    for (i = 0; i < no_of_frames; i++)
         frames[i] = -1;
 
-    printf("\nPage Reference String Processing (FIFO):\n");
-    for (i = 0; i < n; i++) {
-        int flag = 0;
+    printf("\nPage Reference String Processing (LRU):\n");
 
-        // Check if page is already in frame
-        for (j = 0; j < f; j++) {
+    for (i = 0; i < no_of_pages; i++) {
+        flag1 = flag2 = 0;
+
+        // Check if page already in frame
+        for (j = 0; j < no_of_frames; j++) {
             if (frames[j] == pages[i]) {
-                flag = 1;
+                counter++;
+                time[j] = counter; // Update recent use
+                flag1 = flag2 = 1;
                 break;
             }
         }
 
-        // If page not found, replace using FIFO
-        if (flag == 0) {
-            frames[count % f] = pages[i];
-            count++;
-            pf++;
+        // If not in frame
+        if (flag1 == 0) {
+            for (j = 0; j < no_of_frames; j++) {
+                if (frames[j] == -1) {  // Empty slot available
+                    counter++;
+                    page_faults++;
+                    frames[j] = pages[i];
+                    time[j] = counter;
+                    flag2 = 1;
+                    break;
+                }
+            }
         }
 
-        // Print current frame status
+        // If no empty slot, replace LRU page
+        if (flag2 == 0) {
+            pos = findLRU(time, no_of_frames);
+            counter++;
+            page_faults++;
+            frames[pos] = pages[i];
+            time[pos] = counter;
+        }
+
+        // Print current frame state
         printf("Page %d -> ", pages[i]);
-        for (k = 0; k < f; k++) {
-            if (frames[k] != -1)
-                printf("%d ", frames[k]);
+        for (j = 0; j < no_of_frames; j++) {
+            if (frames[j] != -1)
+                printf("%d ", frames[j]);
             else
                 printf("- ");
         }
-        if (flag == 0)
+        if (flag1 == 0)
             printf(" (Page Fault)");
         printf("\n");
     }
 
-    printf("\nTotal Page Faults = %d\n", pf);
+    printf("\nTotal Page Faults = %d\n", page_faults);
 
     return 0;
 }
